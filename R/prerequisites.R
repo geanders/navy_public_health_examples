ff926586c3b0e2b820e2fa6201ae3fc386610f7f
Brooke Anderson <brooke.anderson@colostate.edu>
2019-03-28 12:33:47 -0400

    Add to prerequisites

--- R/prerequisites.R
+++ R/prerequisites.R
@@ -1,5 +1,6 @@
 # You only need to run this once, not every time you use the package.
 install.packages(c("readr", "ggplot2", "forcats", "magrittr", 
                    "dplyr", "lubridate", "sf", "tigris",
-                   "DT", "plotly", "leaflet"))
+                   "DT", "plotly", "leaflet", "flexdashboard", 
+                   "tidyr", "stringr"))
 
